bf0779d617754f89986ea14a197abdc46b25eed0
maxnick83 <maxnick83@ukr.net>
2022-05-04 06:49:40 +0300

    HomeWork3

--- src/Homework3/AttackRate.java
+++ src/Homework3/AttackRate.java
@@ -3,20 +3,20 @@ package Homework3;
 public class AttackRate {
     public static void main(String[] args) {
         //Задача 2. Розрахувати атаки
-        //Армія династії Лі
-        int voinLi = 13;
-        int luchLi = 24;
-        int vershLi = 46;
-        int kolLi = 860; // кількість войнів кожного типу
-        //Армія династія Мінь
-        int voinMin = 9;
-        int luchMin = 35;
-        int vershMin = 12;
-        int kolMin = 1290; // кількість войнів кожного типу(860*1.5)
+        //Армія династії Лі,показник атаки одного воїна
+        float voinLi = 13;
+        float luchLi = 24;
+        float vershLi = 46;
+        float kolLi = 860; // кількість войнів кожного типу
+        //Армія династія Мінь,показник атаки одного воїна
+        float voinMin = 9;
+        float luchMin = 35;
+        float vershMin = 12;
+        float kolMin = 1290; // кількість войнів кожного типу(860*1.5)
         //Розрахунок загального показника кожної армії
-        float zag_Li = (float) kolLi / (voinLi + luchLi + vershLi);
-        float zag_Min = (float) kolMin / (voinMin + luchMin + vershMin);
-        System.out.printf("Показник атаки династії Лі: = %.4f \n", zag_Li);
-        System.out.printf("Показник атаки династії Мінь: = %.4f\n", zag_Min);
+        float zagLi = kolLi * (voinLi+luchLi+vershLi);
+        float zagMin = kolMin * (voinMin+luchMin+vershMin);
+        System.out.printf("Показник атаки династії Лі: = %.4f \n", zagLi);
+        System.out.printf("Показник атаки династії Мінь: = %.4f\n", zagMin);
     }
 }
