a0af8b6ca4f2d460ed0f64360790759c8fde97ab
maxnick83 <maxnick83@ukr.net>
2022-06-11 15:57:20 +0300

    Add Test

---
+++ src/test/java/com/nikitchyn/homework13/task3/AndroidsTest.java
@@ -0,0 +1,12 @@
+package com.nikitchyn.homework13.task3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AndroidsTest {
+
+    @Test
+    public void call() {
+    }
+}
